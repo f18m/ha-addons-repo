00b677c271f500423a06a0cfb87ab84687d4862a
f18m <francesco.montorsi@gmail.com>
2025-06-23 11:45:00 +0200

    bump

--- bump.sh
+++ bump.sh
@@ -1,5 +1,6 @@
 ( cd dnsmasq-dhcp-server && git pull )
 ( cd dnsmasq-dhcp-server-beta && git pull )
 ( cd voip-client && git pull )
+( cd voip-client-beta && git pull )
 git commit -am "bump"
 git push
