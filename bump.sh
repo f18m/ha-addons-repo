a786d85e757e5f21a07e58b8cfb00616bee92975
f18m <francesco.montorsi@gmail.com>
2026-03-02 23:03:30 +0100

    bump

--- bump.sh
+++ bump.sh
@@ -1,6 +1,18 @@
-( cd dnsmasq-dhcp-server && git pull )
-( cd dnsmasq-dhcp-server-beta && git pull )
-( cd voip-client && git pull )
-( cd voip-client-beta && git pull )
+#!/bin/bash
+
+addons=(
+  "dnsmasq-dhcp-server"
+  "dnsmasq-dhcp-server-beta"
+  "voip-client"
+  "voip-client-beta"
+)
+
+for addon in "${addons[@]}"; do
+    @echo "-----------------------------------------------------"
+    @echo "Bumping: $addon"
+    @echo "-----------------------------------------------------"
+    ( cd "$addon" && git pull )
+done
+
 git commit -am "bump"
 git push
